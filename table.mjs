40484ca01d35063220bf765004b763ad53bbe653
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-11-15 15:09:54 -0800

    table

--- table.mjs
+++ table.mjs
@@ -14,14 +14,14 @@ const bits = a => b => {
 }
 
 console.table([
-    bits(1n)(16n),
-    bits(2n)(16n),
-    bits(3n)(16n),
-    bits(4n)(12n),
-    bits(5n)(9n),
-    bits(6n)(8n),
-    bits(7n)(7n),
-    bits(8n)(6n),
-    bits(9n)(5n),
-    bits(10n)(5n),
-])
+    [1n, 16n],
+    [2n, 16n],
+    [3n, 16n],
+    [4n, 12n],
+    [5n, 9n],
+    [6n, 8n],
+    [7n, 7n],
+    [8n, 6n],
+    [9n, 5n],
+    [10n, 5n],
+].map(([a, b]) => bits(a)(b)))
