3798c5a5fe7c720096803b4b74c7f4ba9f1f57f7
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-11-22 16:49:09 -0800

    add_ref, release

--- src/container/ref_.rs
+++ src/container/ref_.rs
@@ -1,12 +1,12 @@
 use std::ops::Deref;
 
-use super::{Container, Info, CLONE, DROP};
+use super::{Container, Info};
 
 pub struct Ref<T: Info>(*mut Container<T>);
 
 impl<T: Info> Ref<T> {
     pub fn new(p: &mut Container<T>) -> Self {
-        unsafe { Container::update::<CLONE>(p) };
+        unsafe { Container::add_ref(p) };
         Self(p)
     }
 }
@@ -20,7 +20,7 @@ impl<T: Info> Clone for Ref<T> {
 impl<T: Info> Drop for Ref<T> {
     fn drop(&mut self) {
         unsafe {
-            Container::update::<DROP>(self.0);
+            Container::release(self.0);
         }
     }
 }
