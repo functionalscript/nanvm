920ed6b448d1c2d9a3be849f78a6e84b76dad104
Shandar Denys <msden.ua@gmail.com>
2023-11-24 20:52:05 +0200

    enums

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,49 @@ enum JsonToken {
     ErrorToken(String),
 }
 
-fn main() {
+enum TokenizerState {
+    Initial,
+    ParseString(Vec<char>),
+    ParseEscapeChar(Vec<char>),
+    //ParseUnicodeChar(Vec<char>), //todo: implement
+    ParseNumber(ParseNumberState),
+    ParseOperator(Vec<char>),
+    ParseMinus,
+}
 
+enum ParseNumberKind {
+    Zero,
+    Int,
+    Dot,
+    Frac,
+    Exp,
+    ExpPlus,
+    ExpMinus,
+    ExpDigits
+}
+
+struct ParseNumberState {
+    kind: ParseNumberKind,
+    value: Vec<char>,
+    s: Sign,
+    m: i128,
+    f: f64,
+    es: Sign,
+    e: f64
+}
+
+enum Sign {
+    Plus,
+    Minus
+}
+
+fn tokenize(input: String) -> Vec<JsonToken> {
+
+    todo!()
+}
+
+fn main() {
+    let res = tokenize(String::from(""));
     //todo:
     //1. read text file to string
     //2. print json tokens from the string
