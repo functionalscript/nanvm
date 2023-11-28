27929d11e367e4f094928822b94c19af655253a8
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-11-28 03:58:06 -0800

    Rc

---
+++ src/container/optional_rc.rs
@@ -0,0 +1,47 @@
+use std::{ptr::read, mem::forget};
+
+use super::{OptionalBase, Update};
+
+#[repr(transparent)]
+pub struct OptionalRc<T: OptionalBase>(T);
+
+impl<T: OptionalBase> OptionalRc<T> {
+    #[inline(always)]
+    pub unsafe fn from_internal(t: T) -> Self {
+        Self(t)
+    }
+    #[inline(always)]
+    pub unsafe fn internal(&self) -> &T {
+        &self.0
+    }
+    #[inline(always)]
+    pub unsafe fn move_to_internal(mut self) -> T {
+        let result = read(&mut self.0);
+        forget(self);
+        result
+    }
+}
+
+impl<T: OptionalBase> Clone for OptionalRc<T> {
+    fn clone(&self) -> Self {
+        unsafe {
+            let result = Self::from_internal(self.0.clone());
+            if let Some(base) = result.0.get_base() {
+                (&mut *base).update(Update::AddRef);
+            }
+            result
+        }
+    }
+}
+
+impl<T: OptionalBase> Drop for OptionalRc<T> {
+    fn drop(&mut self) {
+        unsafe {
+            if let Some(base) = self.0.get_base() {
+                if (&mut *base).update(Update::Release) == 0 {
+                    self.0.dealloc(base)
+                }
+            }
+        }
+    }
+}
--- src/container/rc.rs
+++ src/container/rc.rs
@@ -1,50 +1,6 @@
 use std::{mem::forget, ptr::read};
 
-use super::{optional_base::OptionalBase, Container, Info, Update};
-
-#[repr(transparent)]
-pub struct OptionalRc<T: OptionalBase>(T);
-
-impl<T: OptionalBase> OptionalRc<T> {
-    #[inline(always)]
-    pub unsafe fn from_internal(t: T) -> Self {
-        Self(t)
-    }
-    #[inline(always)]
-    pub unsafe fn internal(&self) -> &T {
-        &self.0
-    }
-    #[inline(always)]
-    pub unsafe fn move_to_internal(mut self) -> T {
-        let result = read(&mut self.0);
-        forget(self);
-        result
-    }
-}
-
-impl<T: OptionalBase> Clone for OptionalRc<T> {
-    fn clone(&self) -> Self {
-        unsafe {
-            let result = Self::from_internal(self.0.clone());
-            if let Some(base) = result.0.get_base() {
-                (&mut *base).update(Update::AddRef);
-            }
-            result
-        }
-    }
-}
-
-impl<T: OptionalBase> Drop for OptionalRc<T> {
-    fn drop(&mut self) {
-        unsafe {
-            if let Some(base) = self.0.get_base() {
-                if (&mut *base).update(Update::Release) == 0 {
-                    self.0.dealloc(base)
-                }
-            }
-        }
-    }
-}
+use super::{optional_base::OptionalBase, Container, Info, Update, optional_rc::OptionalRc};
 
 pub type Rc<T> = OptionalRc<*mut Container<T>>;
 
@@ -53,6 +9,6 @@ impl<T: Info> Rc<T> {
         unsafe { Self::from_internal(Container::alloc(info, i)) }
     }
     pub fn get_items_mut(&self) -> &mut [T::Item] {
-        unsafe { (*self.0).get_items_mut() }
+        unsafe { (**self.internal()).get_items_mut() }
     }
 }
