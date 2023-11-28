00b2a0d00eb46f1a0e8c6d8dbcc67e1d3df34d7d
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-11-27 22:24:33 -0800

    tag_ptr

--- src/value/tag.rs
+++ src/value/tag.rs
@@ -1,7 +1,30 @@
-use crate::common::bit_subset64::BitSubset64;
+use std::mem::forget;
+
+use crate::{
+    common::bit_subset64::BitSubset64,
+    container::{Container, ContainerRef, Info},
+};
 
 pub trait Tag {
     const SUBSET: BitSubset64;
     fn to_unknown_raw(self) -> u64;
     fn from_unknown_raw(u: u64) -> Self;
+}
+
+pub trait TagPtr: Info {
+    const PTR_SUBSET: BitSubset64;
+}
+
+impl<T: TagPtr> Tag for ContainerRef<T> {
+    const SUBSET: BitSubset64 = T::PTR_SUBSET;
+    #[inline(always)]
+    fn to_unknown_raw(self) -> u64 {
+        let p: *mut Container<T> = *self.get();
+        forget(self);
+        p as u64
+    }
+    #[inline(always)]
+    fn from_unknown_raw(u: u64) -> Self {
+        todo!();
+    }
 }
