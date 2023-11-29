1a5b4a80862e83ba7581b0e6c2b9174dfdb98696
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-11-29 00:40:15 -0800

    no clone or copy for Container

--- src/common/allocator.rs
+++ src/common/allocator.rs
@@ -1,18 +1,18 @@
 use std::alloc::{alloc, dealloc, Layout};
 
-pub trait Allocator: Clone + Copy {
-    unsafe fn alloc(self, layout: Layout) -> *mut u8;
-    unsafe fn dealloc(self, ptr: *mut u8, layout: Layout);
+pub trait Allocator {
+    unsafe fn alloc(&mut self, layout: Layout) -> *mut u8;
+    unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout);
 }
 
 #[derive(Clone, Copy)]
 pub struct GlobalAllocator();
 
 impl Allocator for GlobalAllocator {
-    unsafe fn alloc(self, layout: Layout) -> *mut u8 {
+    unsafe fn alloc(&mut self, layout: Layout) -> *mut u8 {
         alloc(layout)
     }
-    unsafe fn dealloc(self, ptr: *mut u8, layout: Layout) {
+    unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
         dealloc(ptr, layout)
     }
 }
