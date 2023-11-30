f58da892ea570c50156a3a8c0dd717c33643fb3a
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-11-30 07:44:27 -0800

    mod

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -9,15 +9,15 @@ use std::alloc::{alloc, dealloc};
 
 use crate::common::usize::max;
 
-// Memory Layout
+// Object properties
 
-trait MemLayout {
+trait Object {
     const ALIGN: usize;
     fn size(&self) -> usize;
     unsafe fn drop_in_place(&mut self);
 }
 
-impl<T> MemLayout for T {
+impl<T> Object for T {
     const ALIGN: usize = align_of::<T>();
     fn size(&self) -> usize {
         size_of::<T>()
@@ -32,15 +32,18 @@ enum Update {
     Release = -1,
 }
 
+// Block header
+
 trait Header {
     unsafe fn update(&self, i: Update) -> isize;
     unsafe fn get<T>(&mut self) -> &mut T;
     unsafe fn delete<T>(&mut self);
 }
 
+/// Block = (Header, Object)
 trait Manager: Sized {
     type Header: Header;
-    unsafe fn new<T: MemLayout, F: FnOnce(*mut T)>(self, size: usize, init: F) -> Ref<T, Self>;
+    unsafe fn new<T: Object, F: FnOnce(*mut T)>(self, size: usize, init: F) -> Ref<T, Self>;
 }
 
 #[repr(transparent)]
@@ -78,20 +81,20 @@ const fn aligned_layout<T>(align: usize) -> Layout {
 
 struct Global();
 
-/// Every type has its own header layout which depends on the type's alignment.
-trait GlobalLayout: MemLayout {
+/// Every object type has its own header layout which depends on the type's alignment.
+trait GlobalLayout: Object {
     const HEADER_LAYOUT: Layout = aligned_layout::<GlobalHeader>(Self::ALIGN);
     const HEADER_LAYOUT_ALIGN: usize = Self::HEADER_LAYOUT.align();
     const HEADER_LAYOUT_SIZE: usize = Self::HEADER_LAYOUT.size();
 }
 
-impl<T: MemLayout> GlobalLayout for T {}
+impl<T: Object> GlobalLayout for T {}
 
 struct GlobalHeader(AtomicIsize);
 
 impl GlobalHeader {
     #[inline(always)]
-    const unsafe fn wrap_layout<T>(size: usize) -> Layout {
+    const unsafe fn block_layout<T>(size: usize) -> Layout {
         Layout::from_size_align_unchecked(T::HEADER_LAYOUT_SIZE + size, T::HEADER_LAYOUT_ALIGN)
     }
 }
@@ -103,20 +106,22 @@ impl Header for GlobalHeader {
     }
     #[inline(always)]
     unsafe fn get<T>(&mut self) -> &mut T {
-        &mut *(self as *mut Self as *mut T).add(T::HEADER_LAYOUT.size())
+        let p = self as *mut Self as *mut u8;
+        let p = p.add(T::HEADER_LAYOUT_SIZE) as *mut T;
+        &mut *p
     }
     unsafe fn delete<T>(&mut self) {
         let p = self.get::<T>();
         let size = p.size();
         drop_in_place(p);
-        dealloc(p as *mut T as *mut u8, Self::wrap_layout::<T>(size));
+        dealloc(p as *mut T as *mut u8, Self::block_layout::<T>(size));
     }
 }
 
 impl Manager for Global {
     type Header = GlobalHeader;
-    unsafe fn new<T: MemLayout, F: FnOnce(*mut T)>(self, size: usize, init: F) -> Ref<T, Self> {
-        let header_p = alloc(Self::Header::wrap_layout::<T>(size)) as *mut Self::Header;
+    unsafe fn new<T: Object, F: FnOnce(*mut T)>(self, size: usize, init: F) -> Ref<T, Self> {
+        let header_p = alloc(Self::Header::block_layout::<T>(size)) as *mut Self::Header;
         *header_p = GlobalHeader(AtomicIsize::new(1));
         init((*header_p).get::<T>());
         Ref(header_p, PhantomData)
