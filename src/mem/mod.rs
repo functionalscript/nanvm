75b5398c4c3d6cad2aecb3108baa85f1ae29a319
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-11-30 15:17:01 -0800

    MyStruct

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -129,3 +129,18 @@ impl Manager for Global {
         Ref(header_p, PhantomData)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::mem::{align_of, size_of};
+
+    struct MyStruct {
+        a: u8,  // 1 byte
+        b: u16, // 2 bytes
+        c: u8,  // 1 byte
+        d: u8,
+    }
+
+    const _: () = assert!(size_of::<MyStruct>() == 6);
+    const _: () = assert!(align_of::<MyStruct>() == 2);
+}
