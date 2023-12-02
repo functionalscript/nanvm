94220733ab648295b100bdd741c16b1cc6b3fce0
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-12-01 19:14:16 -0800

    fixed

--- src/mem/object/fixed.rs
+++ src/mem/object/fixed.rs
@@ -4,3 +4,45 @@ use super::Object;
 pub struct Fixed<T>(pub T);
 
 impl<T> Object for Fixed<T> {}
+
+#[cfg(test)]
+mod test {
+    use core::{
+        mem::forget,
+        sync::atomic::{AtomicIsize, Ordering},
+    };
+
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    use super::{Fixed, *};
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_object() {
+        let a = Fixed(5);
+        assert_eq!(a.size(), 4);
+    }
+
+    struct X<'a>(&'a AtomicIsize);
+
+    impl Object for X<'_> {}
+
+    impl Drop for X<'_> {
+        fn drop(&mut self) {
+            self.0.fetch_add(1, Ordering::Relaxed);
+        }
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_object_drop_in_place() {
+        let a = AtomicIsize::new(5);
+        {
+            let mut x = X(&a);
+            unsafe { x.drop_in_place() };
+            assert_eq!(a.load(Ordering::Relaxed), 6);
+            forget(x);
+        }
+        assert_eq!(a.load(Ordering::Relaxed), 6);
+    }
+}
