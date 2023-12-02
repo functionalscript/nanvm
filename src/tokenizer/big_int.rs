6b5f9ecd76c48a99ddffdbcb3fbf64da22e950a7
Shandar Denys <msden.ua@gmail.com>
2023-12-03 00:27:20 +0200

    big int sum experimental

--- src/tokenizer/big_int.rs
+++ src/tokenizer/big_int.rs
@@ -1,14 +1,79 @@
-use std::ops::Add;
+use std::{iter, ops::Add};
 
+use crate::common::default::default;
+
+#[derive(Debug, PartialEq)]
 struct BigInt {
-    sign: i8,
-    value: Vec<u64>
+    sign: Sign,
+    value: Vec<u64>,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Sign {
+    Positive,
+    Negative,
 }
 
 impl Add for BigInt {
     type Output = BigInt;
 
     fn add(self, rhs: Self) -> Self::Output {
-        todo!()
+        match (self.sign, rhs.sign) {
+            (Sign::Positive, Sign::Positive) => add_same_sign(self, rhs),
+            (Sign::Negative, Sign::Negative) => add_same_sign(self, rhs),
+            _ => todo!(),
+        }
+    }
+}
+
+fn add_same_sign(lhs: BigInt, rhs: BigInt) -> BigInt {
+    let mut result: Vec<_> = default();
+    let mut carry = 0;
+    let iter = match rhs.value.len() > lhs.value.len() {
+        true => rhs
+            .value
+            .iter()
+            .zip(lhs.value.iter().chain(iter::repeat(&0))),
+        false => lhs
+            .value
+            .iter()
+            .zip(rhs.value.iter().chain(iter::repeat(&0))),
+    };
+    for (a, b) in iter {
+        let next = a.wrapping_add(carry).wrapping_add(*b);
+        result.push(next);
+        carry = if next < *a { 1 } else { 0 };
+    }
+    if (carry == 1) {
+        result.push(1);
+    }
+    BigInt {
+        sign: lhs.sign,
+        value: result,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    use crate::common::array::ArrayEx;
+
+    use super::{BigInt, Sign};
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_empty() {
+        let a = BigInt {
+            sign: Sign::Positive,
+            value: [1].vec(),
+        };
+        let b = BigInt {
+            sign: Sign::Positive,
+            value: [2].vec(),
+        };
+        let result = a + b;
+        assert_eq!(result.sign, Sign::Positive);
+        assert_eq!(&result, &BigInt { sign: Sign::Positive, value: [3].vec()});
     }
 }
