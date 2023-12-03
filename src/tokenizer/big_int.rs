6d7e67b31f6b6f236c619db8371f580a9372c566
Shandar Denys <msden.ua@gmail.com>
2023-12-03 21:11:39 +0200

    order

--- src/tokenizer/big_int.rs
+++ src/tokenizer/big_int.rs
@@ -1,14 +1,14 @@
-use std::{iter, ops::Add};
+use std::{iter, ops::Add, cmp::Ordering};
 
 use crate::common::default::default;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 struct BigInt {
     sign: Sign,
     value: Vec<u64>,
 }
 
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Clone, Copy, Eq)]
 enum Sign {
     Positive,
     Negative,
@@ -17,12 +17,55 @@ enum Sign {
 impl Add for BigInt {
     type Output = BigInt;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        match (self.sign, rhs.sign) {
-            (Sign::Positive, Sign::Positive) => add_same_sign(self, rhs),
-            (Sign::Negative, Sign::Negative) => add_same_sign(self, rhs),
-            _ => todo!(),
+    fn add(self, other: Self) -> Self::Output {
+        match self.sign == other.sign {
+            true => add_same_sign(self, other),
+            false => substract_same_sign(self, other),
         }
+    }
+}
+
+impl PartialOrd for Sign {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Sign {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Sign::Positive, Sign::Negative) => Ordering::Greater,
+            (Sign::Negative, Sign::Positive) => Ordering::Less,
+            _ => Ordering::Equal
+        }
+    }
+}
+
+impl PartialOrd for BigInt {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for BigInt {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.sign != other.sign {
+            return self.sign.cmp(&other.sign);
+        }
+
+        let self_len = self.value.len();
+        let other_len = other.value.len();
+        if self_len != other_len {
+            return self_len.cmp(&other_len);
+        }
+
+        for (&self_digit, &other_digit) in self.value.iter().rev().zip(other.value.iter().rev()) {
+            if self_digit != other_digit {
+                return self_digit.cmp(&other_digit);
+            }
+        }
+
+        Ordering::Equal
     }
 }
 
@@ -53,8 +96,14 @@ fn add_same_sign(lhs: BigInt, rhs: BigInt) -> BigInt {
     }
 }
 
+fn substract_same_sign(lhs: BigInt, rhs: BigInt) -> BigInt {
+    todo!()
+}
+
 #[cfg(test)]
 mod test {
+    use std::cmp::Ordering;
+
     use wasm_bindgen_test::wasm_bindgen_test;
 
     use crate::common::array::ArrayEx;
@@ -63,7 +112,51 @@ mod test {
 
     #[test]
     #[wasm_bindgen_test]
-    fn test_add_same_sign() {
+    fn test_ord() {
+        let a = BigInt {
+            sign: Sign::Positive,
+            value: [1].vec(),
+        };
+        let b = BigInt {
+            sign: Sign::Positive,
+            value: [1].vec(),
+        };
+        assert_eq!(a.cmp(&b), Ordering::Equal);
+
+        let a = BigInt {
+            sign: Sign::Positive,
+            value: [1].vec(),
+        };
+        let b = BigInt {
+            sign: Sign::Positive,
+            value: [2].vec(),
+        };
+        assert_eq!(a.cmp(&b), Ordering::Less);
+
+        let a = BigInt {
+            sign: Sign::Positive,
+            value: [1].vec(),
+        };
+        let b = BigInt {
+            sign: Sign::Negative,
+            value: [2].vec(),
+        };
+        assert_eq!(a.cmp(&b), Ordering::Greater);
+
+        let a = BigInt {
+            sign: Sign::Positive,
+            value: [1, 2].vec(),
+        };
+        let b = BigInt {
+            sign: Sign::Positive,
+            value: [2, 1].vec(),
+        };
+        assert_eq!(a.cmp(&b), Ordering::Greater);
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_add() {
         let a = BigInt {
             sign: Sign::Positive,
             value: [1].vec(),
