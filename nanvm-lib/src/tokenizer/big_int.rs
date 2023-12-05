5732d1e9e9ce0bcb5753cdf194f420951d6266ec
Shandar Denys <msden.ua@gmail.com>
2023-12-05 21:04:19 +0200

    fix mul

--- nanvm-lib/src/tokenizer/big_int.rs
+++ nanvm-lib/src/tokenizer/big_int.rs
@@ -117,15 +117,17 @@ impl Mul for &BigInt {
             return BigInt::zero();
         }
         let mut value: Vec<_> = default();
-        let lhs_len = self.value.len();
-        let rhs_len = other.value.len();
-        let total_len = lhs_len + rhs_len - 1;
+        let lhs_max = self.value.len() - 1;
+        let rhs_max = other.value.len() -1 ;
+        let total_max = self.value.len() + other.value.len() - 1;
         let mut i = 0;
-        while i < total_len {
-            let mut j = if i > rhs_len { i - rhs_len } else { 0 };
-            let max = if i < lhs_len { i } else { lhs_len };
+        while i < total_max {
+            let mut j = if i > rhs_max { i - rhs_max } else { 0 };
+            let max = if i < lhs_max { i } else { lhs_max };
             let mut d = 0;
+            println!("i = {} j = {} max = {}", i, j, max);
             while j <= max {
+                println!("i = {} j = {} max = {}", i, j, max);
                 d = d + self.value[j] * other.value[i - j];
                 j = j + 1;
             }
@@ -588,6 +590,23 @@ mod test {
             &BigInt {
                 sign: Sign::Positive,
                 value: [1].vec()
+            },
+        );
+
+        let a = BigInt {
+            sign: Sign::Negative,
+            value: [1, 2, 3, 4].vec(),
+        };
+        let b = BigInt {
+            sign: Sign::Negative,
+            value: [1, 2, 3].vec(),
+        };
+        let result = &a * &b;
+        assert_eq!(
+            &result,
+            &BigInt {
+                sign: Sign::Positive,
+                value: [1, 4, 10, 16, 17, 12].vec()
             },
         );
     }
