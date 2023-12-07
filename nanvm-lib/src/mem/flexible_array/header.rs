963f40fc7a08b9c0e5bfaf267da86c213b4805e2
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-12-06 23:14:26 -0800

    tests

--- nanvm-lib/src/mem/flexible_array/header.rs
+++ nanvm-lib/src/mem/flexible_array/header.rs
@@ -10,3 +10,31 @@ pub trait FlexibleArrayHeader: Sized {
         FlexibleArrayNew::new(self, items)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    use crate::mem::{flexible_array::FlexibleArray, new_in_place::NewInPlace};
+
+    use super::FlexibleArrayHeader;
+
+    struct H();
+
+    impl FlexibleArrayHeader for H {
+        type Item = u8;
+        fn len(&self) -> usize {
+            5
+        }
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test() {
+        let x = H();
+        let y = x.to_new([1, 2, 3, 4, 5].iter().copied());
+        let mut buffer = [0u8; 5];
+        unsafe { y.new_in_place(buffer.as_mut_ptr() as *mut FlexibleArray<_>) };
+        assert_eq!(buffer, [1, 2, 3, 4, 5]);
+    }
+}
