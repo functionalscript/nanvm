70b3056aed0ec2d8162aa6adf6c3f98ab5a7107b
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-12-08 23:21:48 -0800

    arena

--- nanvm-lib/src/mem/arena.rs
+++ nanvm-lib/src/mem/arena.rs
@@ -1,4 +1,4 @@
-use core::{alloc::Layout, cell::Cell, marker::PhantomData};
+use core::{alloc::Layout, cell::Cell};
 
 use super::{
     block::header::BlockHeader,
@@ -7,24 +7,38 @@ use super::{
     ref_::counter_update::RefCounterUpdate,
 };
 
-#[derive(Debug)]
-struct Arena<'a> {
-    begin: Cell<usize>,
-    end: usize,
-    _0: PhantomData<&'a ()>,
+trait Buffer {
+    unsafe fn begin(&self) -> *mut u8;
+    unsafe fn end(&self) -> *mut u8;
 }
 
-impl<'a> Arena<'a> {
+impl Buffer for &mut [u8] {
+    #[inline(always)]
+    unsafe fn begin(&self) -> *mut u8 {
+        self.as_ptr() as *mut u8
+    }
+    #[inline(always)]
+    unsafe fn end(&self) -> *mut u8 {
+        self.as_ptr().add(self.len()) as *mut u8
+    }
+}
+
+#[derive(Debug)]
+struct Arena<T: Buffer> {
+    buffer: T,
+    current: Cell<usize>,
+}
+
+impl<T: Buffer> Arena<T> {
     #[inline(always)]
     const fn layout(size: usize) -> Layout {
         unsafe { Layout::from_size_align_unchecked(size, 1) }
     }
-    pub fn new(range: &'a mut [u8]) -> Self {
-        let begin = range.as_ptr() as usize;
+    pub fn new(buffer: T) -> Self {
+        let current = unsafe { Cell::new(buffer.begin() as usize) };
         Self {
-            begin: Cell::new(begin),
-            end: begin + range.len(),
-            _0: PhantomData,
+            buffer,
+            current,
         }
     }
 }
@@ -39,22 +53,22 @@ impl BlockHeader for NoHeader {
     }
 }
 
-impl<'a> Dealloc for &Arena<'a> {
+impl<T: Buffer> Dealloc for &Arena<T> {
     type BlockHeader = NoHeader;
     #[inline(always)]
     unsafe fn dealloc(_: *mut u8, _: Layout) {}
 }
 
-impl<'a> Manager for &'a Arena<'a> {
+impl<T: Buffer> Manager for &Arena<T> {
     type Dealloc = Self;
     unsafe fn alloc(self, layout: Layout) -> *mut u8 {
         let align = layout.align();
-        let current = align_to(self.begin.get(), align);
+        let current = align_to(self.current.get() as usize, align);
         let end = current + layout.size();
-        if end > self.end {
+        if end > self.buffer.end() as usize {
             panic!("out of memory");
         }
-        self.begin.set(end);
+        self.current.set(end);
         current as *mut u8
     }
 }
@@ -63,7 +77,7 @@ impl<'a> Manager for &'a Arena<'a> {
 mod test {
     use wasm_bindgen_test::wasm_bindgen_test;
 
-    use crate::mem::manager::Manager;
+    use crate::mem::{manager::Manager, arena::Buffer};
 
     use super::Arena;
 
@@ -71,14 +85,14 @@ mod test {
     #[wasm_bindgen_test]
     fn test() {
         let mut range = [0u8; 1024];
-        let arena = Arena::new(&mut range);
-        {
-            assert_eq!(arena.end - arena.begin.get(), 1024);
+        let arena = Arena::new(&mut range[..]);
+        unsafe {
+            assert_eq!(arena.buffer.end() as usize - arena.current.get(), 1024);
             let r = arena.fixed_new(42u8).to_ref();
             let r2 = r.try_to_mut_ref().unwrap_err();
-            assert_eq!(arena.end - arena.begin.get(), 1023);
+            assert_eq!(arena.buffer.end() as usize - arena.current.get(), 1023);
             let mr = arena.fixed_new(43);
-            assert_eq!(arena.end - arena.begin.get(), 1016);
+            assert_eq!(arena.buffer.end() as usize - arena.current.get(), 1016);
         }
     }
 
@@ -86,10 +100,10 @@ mod test {
     #[wasm_bindgen_test]
     fn test_1() {
         let mut range = [0u8; 1];
-        let arena = Arena::new(&mut range);
-        assert_eq!(arena.end - arena.begin.get(), 1);
+        let arena = Arena::new(&mut range[..]);
+        unsafe { assert_eq!(arena.buffer.end() as usize - arena.current.get(), 1); }
         let r = arena.fixed_new(42u8).to_ref();
-        assert_eq!(arena.end, arena.begin.get());
+        unsafe { assert_eq!(arena.buffer.end() as usize, arena.current.get()); }
     }
 
     #[test]
@@ -97,7 +111,7 @@ mod test {
     #[wasm_bindgen_test]
     fn test_out_of_memory() {
         let mut range = [0u8; 1];
-        let arena = Arena::new(&mut range);
+        let arena = Arena::new(&mut range[..]);
         let r = arena.fixed_new(42u8).to_ref();
         let r2 = arena.fixed_new(42u8).to_ref();
     }
