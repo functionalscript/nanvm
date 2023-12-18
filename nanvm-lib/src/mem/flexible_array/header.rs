0818bde707b4074187daf788b0f37293761025ea
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-12-18 08:51:00 -0800

    header

--- nanvm-lib/src/mem/flexible_array/header.rs
+++ nanvm-lib/src/mem/flexible_array/header.rs
@@ -13,11 +13,18 @@ pub trait FlexibleArrayHeader: Sized {
     }
 }
 
+impl FlexibleArrayHeader for usize {
+    #[inline(always)]
+    fn len(&self) -> usize {
+        *self
+    }
+}
+
 #[cfg(test)]
 mod test {
     use wasm_bindgen_test::wasm_bindgen_test;
 
-    use crate::mem::{constructor::Constructor, flexible_array::FlexibleArray};
+    use crate::mem::{constructor::Constructor, object::Object, flexible_array::{FlexibleArray, constructor::FlexibleArrayConstructor}};
 
     use super::FlexibleArrayHeader;
 
@@ -37,5 +44,40 @@ mod test {
         let mut buffer = [0u8; 5];
         unsafe { y.construct(buffer.as_mut_ptr() as *mut FlexibleArray<H, u8>) };
         assert_eq!(buffer, [1, 2, 3, 4, 5]);
+    }
+
+    #[repr(C)]
+    struct Item(*mut u8);
+
+    impl Drop for Item {
+        fn drop(&mut self) {
+            unsafe {
+                *self.0 += 1;
+            }
+        }
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_len() {
+        let mut buffer = [0usize; 5];
+
+        let mut i: u8 = 0;
+        {
+            let p = &mut i as *mut _;
+            let v = [Item(p), Item(p), Item(p), Item(p)];
+            let x: FlexibleArrayConstructor<_, _> = v.into_iter().into();
+            unsafe { x.construct(buffer.as_ptr() as *mut _) }
+            assert_eq!(i, 0);
+            assert_eq!(buffer[0], 4);
+            assert_eq!(p, buffer[1] as *mut _);
+            assert_eq!(p, buffer[2] as *mut _);
+            assert_eq!(p, buffer[3] as *mut _);
+            assert_eq!(p, buffer[4] as *mut _);
+            let px = buffer.as_mut_ptr() as *mut FlexibleArray<usize, Item>;
+            unsafe { (*px).object_drop() };
+            assert_eq!(i, 4);
+        }
+        assert_eq!(i, 4);
     }
 }
