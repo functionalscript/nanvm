c96d808e35466252c9ca5c2a59948a985cf030ec
sergey-shandar <sergey-shandar@users.noreply.github.com>
2023-12-21 12:25:46 -0800

    any_internal

--- nanvm-lib/src/js/any_cast.rs
+++ nanvm-lib/src/js/any_cast.rs
@@ -6,9 +6,9 @@ use crate::{
 use super::{any::Any, any_internal::AnyInternal, ref_cast::RefCast, value_cast::ValueCast};
 
 pub trait AnyCast<D: Dealloc>: Sized {
-    unsafe fn is_type_of(u: u64) -> bool;
+    unsafe fn is_type_of(any_internal: u64) -> bool;
     unsafe fn move_to_any_internal(self) -> u64;
-    unsafe fn from_any_internal(set: u64) -> Self;
+    unsafe fn from_any_internal(any_internal: u64) -> Self;
     //
     #[inline(always)]
     fn move_to_any(self) -> Any<D> {
@@ -21,30 +21,30 @@ where
     u64: Cast<T>,
 {
     #[inline(always)]
-    unsafe fn is_type_of(u: u64) -> bool {
-        T::SUBSET.has(u)
+    unsafe fn is_type_of(any_internal: u64) -> bool {
+        T::SUBSET.has(any_internal)
     }
     #[inline(always)]
     unsafe fn move_to_any_internal(self) -> u64 {
         T::SUBSET.from_value_typed(self)
     }
     #[inline(always)]
-    unsafe fn from_any_internal(set: u64) -> Self {
-        T::SUBSET.get_value_typed(set)
+    unsafe fn from_any_internal(any_internal: u64) -> Self {
+        T::SUBSET.get_value_typed(any_internal)
     }
 }
 
 impl<D: Dealloc, T: RefCast<D>> AnyCast<D> for Ref<T, D> {
     #[inline(always)]
-    unsafe fn is_type_of(u: u64) -> bool {
-        T::REF_SUBSET.has(u)
+    unsafe fn is_type_of(any_internal: u64) -> bool {
+        T::REF_SUBSET.has(any_internal)
     }
     #[inline(always)]
     unsafe fn move_to_any_internal(self) -> u64 {
         T::REF_SUBSET.from_value_typed(self.move_to_internal())
     }
     #[inline(always)]
-    unsafe fn from_any_internal(set: u64) -> Self {
-        Self::from_internal(T::REF_SUBSET.get_value_typed(set))
+    unsafe fn from_any_internal(any_internal: u64) -> Self {
+        Self::from_internal(T::REF_SUBSET.get_value_typed(any_internal))
     }
 }
