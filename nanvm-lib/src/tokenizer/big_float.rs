4c8e79c415188d3444d9b2504b5e54752991af8a
Trinidadec <msden.ua@gmail.com>
2024-01-04 22:53:40 +0200

    Bigfloat to f64 (#56)
    * convert zero
    
    * normal number bits
    
    * fix decreasing significand
    
    * minor
    
    * fix normal f64 exp
    
    * test
    
    * tests
    
    * f64 infinity
    
    * subnormal bits
    
    * fix
    
    * fmt
    
    * constants
    
    * remove unused
    
    * additional tests
    
    * non_zero_reminder

--- nanvm-lib/src/tokenizer/big_float.rs
+++ nanvm-lib/src/tokenizer/big_float.rs
@@ -1,4 +1,7 @@
-use crate::{common::cast::Cast, tokenizer::big_uint::BigUint};
+use crate::{
+    common::cast::Cast,
+    tokenizer::{big_int::Sign, big_uint::BigUint},
+};
 
 use super::big_int::BigInt;
 
@@ -6,6 +9,7 @@ use super::big_int::BigInt;
 pub struct BigFloat<const BASE: u32> {
     pub significand: BigInt,
     pub exp: i64,
+    //pub non_zero_reminder: bool
 }
 
 impl<const BASE: u32> BigFloat<BASE> {
@@ -14,13 +18,13 @@ impl<const BASE: u32> BigFloat<BASE> {
         exp: 0,
     };
 
-    fn increase_significand(&mut self, min_significand: BigUint) {
+    fn increase_significand(&mut self, min_significand: &BigUint) {
         if self.significand.is_zero() {
             return;
         }
 
         loop {
-            if self.significand.value >= min_significand {
+            if self.significand.value >= *min_significand {
                 return;
             }
             self.significand.value = &self.significand.value << &BigUint::one();
@@ -35,7 +39,7 @@ impl<const BASE: u32> BigFloat<BASE> {
 
         let mut are_bits_lost = false;
         loop {
-            if self.significand.value <= *max_significand {
+            if self.significand.value < *max_significand {
                 break;
             }
             let lastBit = self.significand.value.get_last_bit();
@@ -56,13 +60,6 @@ impl BigFloat<10> {
             return BigFloat::ZERO;
         }
 
-        if self.exp == 0 {
-            return BigFloat {
-                significand: self.significand,
-                exp: 0,
-            };
-        }
-
         let five = BigUint { value: [5].cast() };
         if self.exp > 0 {
             let new_sign = &self.significand * &five.pow_u64(self.exp as u64).to_big_int();
@@ -76,7 +73,7 @@ impl BigFloat<10> {
         let p = five.pow_u64(-self.exp as u64);
         let mut bf10 = self.clone();
         let min_significand = &BigUint::one() << &BigUint::from_u64(precision as u64);
-        bf10.increase_significand(&p * &min_significand);
+        bf10.increase_significand(&(&p * &min_significand));
 
         let (q, r) = bf10.significand.div_mod(&p.to_big_int());
         let mut bf2: BigFloat<2> = BigFloat {
@@ -109,6 +106,54 @@ impl BigFloat<10> {
                 sign: bf2.significand.sign,
             },
             exp,
+        }
+    }
+}
+
+impl BigFloat<2> {
+    fn to_f64(self) -> f64 {
+        f64::from_bits(self.get_f64_bits())
+    }
+
+    fn get_f64_bits(self) -> u64 {
+        const PRECISION: u64 = 52;
+        const FRAC_MASK: u64 = (1 << PRECISION) - 1;
+
+        let mut bits: u64 = 0;
+        if self.significand.sign == Sign::Negative {
+            bits = bits | 1 << 63;
+        }
+
+        if self.significand.is_zero() {
+            return bits;
+        }
+
+        let mut value = self.clone();
+        let min_significand = &BigUint::one() << &BigUint::from_u64(PRECISION);
+        value.increase_significand(&min_significand);
+        let max_significand = &min_significand << &BigUint::one();
+        value.decrease_significand(&max_significand);
+
+        let f64_exp = value.exp + PRECISION as i64;
+        match f64_exp {
+            -1022..=1023 => {
+                let exp_bits = (f64_exp + 1023) as u64;
+                bits = bits | exp_bits << 52;
+                let frac_bits = value.significand.value.value[0] & FRAC_MASK;
+                bits = bits | frac_bits;
+                bits
+            }
+            -1074..=-1023 => {
+                let exp_dif = -1022 - f64_exp;
+                let frac_bits = value.significand.value.value[0] >> exp_dif;
+                bits = bits | frac_bits;
+                bits
+            }
+            exp if exp > 1023 => {
+                bits = bits | 2047 << 52;
+                bits
+            }
+            _ => bits,
         }
     }
 }
@@ -155,7 +200,7 @@ mod test {
             significand: BigInt::from_i64(100),
             exp: 0,
         };
-        let res = a.to_bin(64);
+        let res = a.to_bin(7);
         assert_eq!(
             res,
             BigFloat {
@@ -222,6 +267,29 @@ mod test {
                     }
                 },
                 exp: -60,
+            }
+        );
+
+        let a = BigFloat {
+            significand: BigInt {
+                sign: Sign::Positive,
+                value: BigUint {
+                    value: [0, 1].cast(),
+                },
+            },
+            exp: 0,
+        };
+        let res = a.to_bin(53);
+        assert_eq!(
+            res,
+            BigFloat {
+                significand: BigInt {
+                    sign: Sign::Positive,
+                    value: BigUint {
+                        value: [1 << 52].cast()
+                    }
+                },
+                exp: 12,
             }
         );
     }
@@ -336,5 +404,137 @@ mod test {
                 exp: 2,
             }
         );
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_zero_to_f64() {
+        let a = BigFloat {
+            significand: BigInt::ZERO,
+            exp: 100,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 0.0);
+        assert!(res.is_sign_positive());
+
+        let a = BigFloat {
+            significand: BigInt {
+                sign: Sign::Negative,
+                value: BigUint::ZERO,
+            },
+            exp: 100,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 0.0);
+        assert!(res.is_sign_negative());
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_normal_to_f64() {
+        let a = BigFloat {
+            significand: BigInt::from_u64(1),
+            exp: 0,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 1.0);
+
+        let a = BigFloat {
+            significand: BigInt::from_i64(-3),
+            exp: -1,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, -1.5);
+
+        let a = BigFloat {
+            significand: BigInt::from_i64(1),
+            exp: -1022,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 2.0f64.powf(-1022.0));
+
+        let a = BigFloat {
+            significand: BigInt::from_i64(1),
+            exp: 1023,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 2.0f64.powf(1023.0));
+
+        let a = BigFloat {
+            significand: BigInt::from_u64((1 << 52) - 1),
+            exp: 0,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 4503599627370495f64);
+
+        let a = BigFloat {
+            significand: BigInt::from_u64((1 << 53) - 1),
+            exp: 0,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 9007199254740991f64);
+
+        // let a = BigFloat {
+        //     significand: BigInt::from_u64((1 << 54) - 1),
+        //     exp: 0,
+        // };
+        // let res = a.to_f64();
+        // assert_eq!(res, 18014398509481983f64);
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_infinity_to_f64() {
+        let a = BigFloat {
+            significand: BigInt::from_i64(1),
+            exp: 1024,
+        };
+        let res = a.to_f64();
+        assert!(res.is_infinite());
+        assert!(res.is_sign_positive());
+
+        let a = BigFloat {
+            significand: BigInt::from_i64(-1),
+            exp: 1024,
+        };
+        let res = a.to_f64();
+        assert!(res.is_infinite());
+        assert!(res.is_sign_negative());
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_subnormal_to_f64() {
+        let a = BigFloat {
+            significand: BigInt::from_u64(1),
+            exp: -1023,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 2.0f64.powf(-1023.0));
+        assert!(res.is_subnormal());
+
+        let a = BigFloat {
+            significand: BigInt::from_i64(-1),
+            exp: -1023,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, -2.0f64.powf(-1023.0));
+        assert!(res.is_subnormal());
+
+        let a = BigFloat {
+            significand: BigInt::from_u64(1),
+            exp: -1074,
+        };
+        let res = a.to_f64();
+        assert_eq!(res.to_bits(), 1);
+        assert_eq!(res, 2.0f64.powf(-1074.0));
+        assert!(res.is_subnormal());
+
+        let a = BigFloat {
+            significand: BigInt::from_u64(1),
+            exp: -1075,
+        };
+        let res = a.to_f64();
+        assert_eq!(res, 0.0);
     }
 }
