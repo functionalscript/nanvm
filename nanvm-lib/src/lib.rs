acf39891e9aadd1ce77cb90d39d8e1a648410522
Shandar Denys <msden.ua@gmail.com>
2024-01-10 20:36:55 +0200

    parse state

--- nanvm-lib/src/lib.rs
+++ nanvm-lib/src/lib.rs
@@ -2,5 +2,5 @@ pub mod big_numbers;
 mod common;
 pub mod js;
 pub mod mem;
-pub mod tokenizer;
 pub mod parser;
+pub mod tokenizer;
--- nanvm-lib/src/parser/mod.rs
+++ nanvm-lib/src/parser/mod.rs
@@ -6,15 +6,39 @@ pub enum JsonUnknown {
     String(String),
     Number(f64),
     Null,
-    Array(Vec<JsonUnknown>)
+    Array(Vec<JsonUnknown>),
 }
 
 pub enum JsonStackElement {
     Object(JsonStackObject),
-    Array(Vec<JsonUnknown>)
+    Array(Vec<JsonUnknown>),
 }
 
-pub struct  JsonStackObject {
+pub struct JsonStackObject {
     pub map: HashMap<String, JsonUnknown>,
     pub key: String,
+}
+
+pub enum ParseStatus {
+    Initial,
+    ArrayStart,
+    ArrayValue,
+    ArrayComma,
+    ObjectStart,
+    ObjectKey,
+    ObjectColon,
+    ObjectValue,
+    ObjectComma,
+}
+
+pub struct StateParse {
+    pub status: ParseStatus,
+    pub top: Option<JsonStackElement>,
+    pub stack: JsonStackElement,
+}
+
+pub enum JsonState {
+    Parse(StateParse),
+    Result(JsonUnknown),
+    Error(String),
 }
