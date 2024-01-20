5a600ddc718acc1ec654585454017cec2dda3532
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2024-01-20 12:11:28 -0800

    d.js samples

---
+++ notes/samle.d.mjs
@@ -0,0 +1,11 @@
+// const null = null
+// const true = true
+// const false = false
+const undefined = void 0
+const NaN = 0/0
+
+export default {
+    null: null,
+    true: true,
+    export: "export", // export,
+}
