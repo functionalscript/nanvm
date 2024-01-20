ece9d94b057838b88747327da4e674cd008cf51f
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2024-01-20 12:19:30 -0800

    d.js samples (#80)
    * d.js samples
    
    * keywords

---
+++ notes/samle.d.mjs
@@ -0,0 +1,15 @@
+// const null = null
+// const true = true
+// const false = false
+const undefined = void 0
+const NaN = 0/0
+// const const = "const"
+// const export = "export"
+// const default = "default"
+
+export default {
+    null: null,
+    true: true,
+    export: "export",
+    const: "const",
+}
