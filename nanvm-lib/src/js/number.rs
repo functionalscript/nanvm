f447e5791ac35336d61570b0206ae4ba1e8339f9
Sasha Gil <alexgil@microsoft.com>
2024-01-26 22:07:25 -0800

    Address a CR comment

--- nanvm-lib/src/js/number.rs
+++ nanvm-lib/src/js/number.rs
@@ -43,6 +43,7 @@ pub mod test {
 
     #[test]
     #[wasm_bindgen_test]
+    #[allow(clippy::zero_divided_by_zero)]
     fn test_check() {
         assert!(is_valid(0));
         assert!(is_valid(1));
@@ -50,6 +51,7 @@ pub mod test {
         assert!(is_valid(NAN));
         assert!(is_valid(NEG_INFINITY));
         assert_eq!(f64::NAN.to_bits(), NAN);
+        assert_eq!((0.0f64 / 0.0).to_bits(), NAN);
     }
 
     #[test]
