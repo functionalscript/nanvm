2f293f85dba6abb2d027f50980bb5534d8f92e45
Shandar Denys <msden.ua@gmail.com>
2024-01-28 22:04:44 +0200

    test with error

--- nanvm-lib/src/parser/mod.rs
+++ nanvm-lib/src/parser/mod.rs
@@ -629,6 +629,11 @@ mod test {
         let items = result_unwrap.items();
         let item0 = items[0].clone();
         assert_eq!(item0.try_move(), Ok(2.0));
+
+        let json_str = include_str!("../../test/test-const-error.d.cjs");
+        let tokens = tokenize(json_str.to_owned());
+        let result = parse(manager, tokens.into_iter());
+        assert!(result.is_err());
     }
 
     #[test]
