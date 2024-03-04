99dd4f69020e128cd6a88043cc6bd9d00bb0d087
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-03 19:21:43 -0800

    to_json

--- nanvm-lib/src/js/visitor.rs
+++ nanvm-lib/src/js/visitor.rs
@@ -4,7 +4,7 @@ use super::{
     any::Any, js_array::JsArrayRef, js_object::JsObjectRef, js_string::JsStringRef, type_::Type,
 };
 
-enum Visitor<T: Dealloc> {
+pub enum Visitor<T: Dealloc> {
     Number(f64),
     Null,
     Bool(bool),
@@ -13,7 +13,7 @@ enum Visitor<T: Dealloc> {
     Array(JsArrayRef<T>),
 }
 
-fn to_visitor<T: Dealloc>(any: Any<T>) -> Visitor<T> {
+pub fn to_visitor<T: Dealloc>(any: Any<T>) -> Visitor<T> {
     match any.get_type() {
         Type::Number => Visitor::Number(any.try_move().unwrap()),
         Type::Null => Visitor::Null,
--- nanvm-lib/src/serializer.rs
+++ nanvm-lib/src/serializer.rs
@@ -1,5 +1,50 @@
-use crate::{common::default::default, js::any::Any, mem::manager::Dealloc};
+use crate::{
+    js::{
+        any::Any,
+        js_string::JsStringRef,
+        visitor::{to_visitor, Visitor},
+    },
+    mem::manager::Dealloc,
+};
 
-pub fn to_json(array: &Any<impl Dealloc>) -> String {
-    default()
+pub fn to_string(s: JsStringRef<impl Dealloc>) -> String {
+    let mut r = String::new();
+    r.push('"');
+    for &c in s.items().iter() {
+        // TODO: escape
+        r.push(c as u8 as _);
+    }
+    r.push('"');
+    r
+}
+
+pub fn to_json(any: Any<impl Dealloc>) -> String {
+    match to_visitor(any) {
+        Visitor::Number(n) => n.to_string(),
+        Visitor::Null => "null".to_string(),
+        Visitor::Bool(b) => b.to_string(),
+        Visitor::String(s) => to_string(s),
+        Visitor::Object(o) => {
+            let mut r = "{".to_string();
+            for (k, v) in o.items().iter() {
+                r.push_str(&to_string(k.clone()));
+                r.push(':');
+                r.push_str(&to_json(v.clone()));
+                r.push(',');
+            }
+            r.pop();
+            r.push('}');
+            r
+        }
+        Visitor::Array(a) => {
+            let mut r = "[".to_string();
+            for v in a.items().iter() {
+                r.push_str(&to_json(v.clone()));
+                r.push(',');
+            }
+            r.pop();
+            r.push(']');
+            r
+        }
+    }
 }
