e5e1d6909436d33c4964fa934676e9d2d2b288e0
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-03 22:04:31 -0800

    write_str

--- nanvm-lib/src/serializer.rs
+++ nanvm-lib/src/serializer.rs
@@ -13,47 +13,65 @@ use crate::{
 
 use core::fmt::{self, Write};
 
-fn write_js_string(s: &JsStringRef<impl Dealloc>, r: &mut impl Write) -> fmt::Result {
-    r.write_char('"')?;
-    for &c in s.items().iter() {
-        // TODO: escape
-        r.write_char(c as u8 as _)?;
+trait WriteJson: Write {
+    fn write_js_string(&mut self, s: &JsStringRef<impl Dealloc>) -> fmt::Result {
+        self.write_char('"')?;
+        for &c in s.items().iter() {
+            // TODO: escape
+            self.write_char(c as u8 as _)?;
+        }
+        self.write_char('"')
     }
-    r.write_char('"')
+
+    fn write_list<I>(
+        &mut self,
+        open: char,
+        close: char,
+        v: Ref<FlexibleArray<I, impl FlexibleArrayHeader>, impl Dealloc>,
+        f: impl Fn(&mut Self, &I) -> fmt::Result,
+    ) -> fmt::Result {
+        let mut comma = "";
+        self.write_char(open)?;
+        for i in v.items().iter() {
+            self.write_str(comma)?;
+            f(self, i)?;
+            comma = ",";
+        }
+        self.write_char(close)
+    }
+
+    fn write_json(&mut self, any: Any<impl Dealloc>) -> fmt::Result {
+        match to_visitor(any) {
+            Visitor::Number(n) => self.write_str(n.to_string().as_str()),
+            Visitor::Null => self.write_str("null"),
+            Visitor::Bool(b) => self.write_str(if b { "true" } else { "false" }),
+            Visitor::String(s) => self.write_js_string(&s),
+            Visitor::Object(o) => self.write_list('{', '}', o, |w, (k, v)| {
+                w.write_js_string(k)?;
+                w.write_char(':')?;
+                w.write_json(v.clone())
+            }),
+            Visitor::Array(a) => self.write_list('[', ']', a, |w, i| w.write_json(i.clone())),
+        }
+    }
 }
 
-fn write_list<I, W: Write>(
-    mut open: char,
-    close: char,
-    v: Ref<FlexibleArray<I, impl FlexibleArrayHeader>, impl Dealloc>,
-    f: impl Fn(&I, &mut W) -> fmt::Result,
-    r: &mut W,
-) -> fmt::Result {
-    for i in v.items().iter() {
-        r.write_char(open)?;
-        f(i, r)?;
-        open = ',';
-    }
-    r.write_char(close)
-}
+impl<T: Write> WriteJson for T {}
 
-pub fn write_json(any: Any<impl Dealloc>, r: &mut String) -> fmt::Result {
-    match to_visitor(any) {
-        Visitor::Number(n) => r.write_str(n.to_string().as_str()),
-        Visitor::Null => r.write_str("null"),
-        Visitor::Bool(b) => r.write_str(if b { "true" } else { "false" }),
-        Visitor::String(s) => write_js_string(&s, r),
-        Visitor::Object(o) => write_list(
-            '{',
-            '}',
-            o,
-            |(k, v), r| {
-                write_js_string(k, r)?;
-                r.write_char(':')?;
-                write_json(v.clone(), r)
-            },
-            r,
-        ),
-        Visitor::Array(a) => write_list('[', ']', a, |i, r| write_json(i.clone(), r), r),
+
+#[cfg(test)]
+mod test {
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    use crate::{js::{any::Any, js_array::new_array}, mem::global::{Global, GLOBAL}, serializer::WriteJson};
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test() {
+         type A = Any<Global>;
+        let a = new_array(GLOBAL, [].into_iter());
+        let mut s = String::new();
+        s.write_json(A::move_from(a.to_ref())).unwrap();
+        assert_eq!(s, "[]");
     }
 }
