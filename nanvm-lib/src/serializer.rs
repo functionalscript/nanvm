0620b352a4698b01a262d865ce1ea8abb36a5246
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-03 21:40:11 -0800

    fmt

--- nanvm-lib/src/serializer.rs
+++ nanvm-lib/src/serializer.rs
@@ -11,49 +11,49 @@ use crate::{
     },
 };
 
-fn push_js_string(s: &JsStringRef<impl Dealloc>, r: &mut String) {
-    r.push('"');
+use core::fmt::{self, Write};
+
+fn write_js_string(s: &JsStringRef<impl Dealloc>, r: &mut impl Write) -> fmt::Result {
+    r.write_char('"')?;
     for &c in s.items().iter() {
         // TODO: escape
-        r.push(c as u8 as _);
+        r.write_char(c as u8 as _)?;
     }
-    r.push('"');
+    r.write_char('"')
 }
 
-fn push_list<I>(
+fn write_list<I, W: Write>(
     mut open: char,
     close: char,
     v: Ref<FlexibleArray<I, impl FlexibleArrayHeader>, impl Dealloc>,
-    f: impl Fn(&I, &mut String),
-    r: &mut String,
-) {
+    f: impl Fn(&I, &mut W) -> fmt::Result,
+    r: &mut W,
+) -> fmt::Result {
     for i in v.items().iter() {
-        r.push(open);
-        f(i, r);
+        r.write_char(open)?;
+        f(i, r)?;
         open = ',';
     }
-    r.push(close);
+    r.write_char(close)
 }
 
-pub fn push_json(any: Any<impl Dealloc>, r: &mut String) {
+pub fn write_json(any: Any<impl Dealloc>, r: &mut String) -> fmt::Result {
     match to_visitor(any) {
-        Visitor::Number(n) => r.push_str(n.to_string().as_str()),
-        Visitor::Null => r.push_str("null"),
-        Visitor::Bool(b) => r.push_str(if b { "true" } else { "false" }),
-        Visitor::String(s) => push_js_string(&s, r),
-        Visitor::Object(o) => {
-            push_list(
-                '{',
-                '}',
-                o,
-                |(k, v), r| {
-                    push_js_string(k, r);
-                    r.push(':');
-                    push_json(v.clone(), r);
-                },
-                r,
-            );
-        }
-        Visitor::Array(a) => push_list('[', ']', a, |i, r| push_json(i.clone(), r), r),
+        Visitor::Number(n) => r.write_str(n.to_string().as_str()),
+        Visitor::Null => r.write_str("null"),
+        Visitor::Bool(b) => r.write_str(if b { "true" } else { "false" }),
+        Visitor::String(s) => write_js_string(&s, r),
+        Visitor::Object(o) => write_list(
+            '{',
+            '}',
+            o,
+            |(k, v), r| {
+                write_js_string(k, r)?;
+                r.write_char(':')?;
+                write_json(v.clone(), r)
+            },
+            r,
+        ),
+        Visitor::Array(a) => write_list('[', ']', a, |i, r| write_json(i.clone(), r), r),
     }
 }
