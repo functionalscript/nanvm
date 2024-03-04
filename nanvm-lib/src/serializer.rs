ae444799ae7678622372db3ba361ffda940ac527
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-03 20:14:13 -0800

    push_list

--- nanvm-lib/src/serializer.rs
+++ nanvm-lib/src/serializer.rs
@@ -1,12 +1,20 @@
+use core::ops::Deref;
+
 use crate::{
-    common::default::default, js::{
+    common::default::default,
+    js::{
         any::Any,
         js_string::JsStringRef,
         visitor::{to_visitor, Visitor},
-    }, mem::manager::Dealloc
+    },
+    mem::{
+        flexible_array::{header::FlexibleArrayHeader, FlexibleArray},
+        manager::Dealloc,
+        ref_::Ref,
+    },
 };
 
-pub fn push_js_string(s: &JsStringRef<impl Dealloc>, r: &mut String) {
+fn push_js_string(s: &JsStringRef<impl Dealloc>, r: &mut String) {
     r.push('"');
     for &c in s.items().iter() {
         // TODO: escape
@@ -15,37 +23,41 @@ pub fn push_js_string(s: &JsStringRef<impl Dealloc>, r: &mut String) {
     r.push('"');
 }
 
-pub fn to_json(any: Any<impl Dealloc>) -> String {
+fn push_list<I>(
+    mut open: char,
+    close: char,
+    v: Ref<FlexibleArray<I, impl FlexibleArrayHeader>, impl Dealloc>,
+    f: impl Fn(&I, &mut String),
+    r: &mut String,
+) {
+    for i in v.items().iter() {
+        r.push(open);
+        f(i, r);
+        open = ',';
+    }
+    r.push(close);
+}
+
+pub fn to_json(any: Any<impl Dealloc>, r: &mut String) {
     match to_visitor(any) {
-        Visitor::Number(n) => n.to_string(),
-        Visitor::Null => "null".to_string(),
-        Visitor::Bool(b) => b.to_string(),
-        Visitor::String(s) => {
-            let mut r = default();
-            push_js_string(&s, &mut r);
-            r
-        }
+        Visitor::Number(n) => r.push_str(n.to_string().as_str()),
+        Visitor::Null => r.push_str("null"),
+        Visitor::Bool(b) => r.push_str(if b { "true" } else { "false" }),
+        Visitor::String(s) => push_js_string(&s, r),
         Visitor::Object(o) => {
-            let mut r = "{".to_string();
-            for (k, v) in o.items().iter() {
-                push_js_string(k, &mut r);
-                r.push(':');
-                r.push_str(&to_json(v.clone()));
-                r.push(',');
-            }
-            r.pop();
-            r.push('}');
-            r
+            let mut r = default();
+            push_list(
+                '{',
+                '}',
+                o,
+                |kv, r| {
+                    push_js_string(&kv.0, r);
+                    r.push(':');
+                    to_json(kv.1.clone(), r);
+                },
+                &mut r,
+            );
         }
-        Visitor::Array(a) => {
-            let mut r = "[".to_string();
-            for v in a.items().iter() {
-                r.push_str(&to_json(v.clone()));
-                r.push(',');
-            }
-            r.pop();
-            r.push(']');
-            r
-        }
+        Visitor::Array(a) => push_list('[', ']', a, |i, r| to_json(i.clone(), r), r),
     }
 }
