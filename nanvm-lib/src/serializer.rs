270868de774289645c1a02589b10be83a6119cf0
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-03-03 19:32:38 -0800

    serializer

--- nanvm-lib/src/serializer.rs
+++ nanvm-lib/src/serializer.rs
@@ -1,21 +1,18 @@
 use crate::{
-    js::{
+    common::default::default, js::{
         any::Any,
         js_string::JsStringRef,
         visitor::{to_visitor, Visitor},
-    },
-    mem::manager::Dealloc,
+    }, mem::manager::Dealloc
 };
 
-pub fn to_string(s: JsStringRef<impl Dealloc>) -> String {
-    let mut r = String::new();
+pub fn push_js_string(s: &JsStringRef<impl Dealloc>, r: &mut String) {
     r.push('"');
     for &c in s.items().iter() {
         // TODO: escape
         r.push(c as u8 as _);
     }
     r.push('"');
-    r
 }
 
 pub fn to_json(any: Any<impl Dealloc>) -> String {
@@ -23,11 +20,15 @@ pub fn to_json(any: Any<impl Dealloc>) -> String {
         Visitor::Number(n) => n.to_string(),
         Visitor::Null => "null".to_string(),
         Visitor::Bool(b) => b.to_string(),
-        Visitor::String(s) => to_string(s),
+        Visitor::String(s) => {
+            let mut r = default();
+            push_js_string(&s, &mut r);
+            r
+        }
         Visitor::Object(o) => {
             let mut r = "{".to_string();
             for (k, v) in o.items().iter() {
-                r.push_str(&to_string(k.clone()));
+                push_js_string(k, &mut r);
                 r.push(':');
                 r.push_str(&to_json(v.clone()));
                 r.push(',');
