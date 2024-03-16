bdecdd263a2e81759684088b196bbf8c42cd8420
Shandar Denys <msden.ua@gmail.com>
2024-03-16 20:21:57 +0200

    path normalize

---
+++ nanvm-lib/src/parser/path.rs
@@ -0,0 +1,43 @@
+pub fn normalize(path: &str) -> String {
+    let path_split: Vec<_> = path.split("/").collect();
+    let mut result_split: Vec<&str> = Vec::new();
+    for &dir in path_split.iter() {
+        match dir {
+            ".." => {
+                let last = result_split.last();
+                match last 
+                {
+                    Some(x) if !x.eq(&"..") => {
+                        result_split.pop();
+                    },
+                    _ => {
+                        result_split.push(dir);
+                    }
+                    
+                }
+                
+            }
+            _ => {
+                result_split.push(dir);
+            }
+        }
+    }
+    result_split.join("/")
+}
+
+#[cfg(test)]
+mod test {
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    use crate::parser::path::normalize;
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test() {
+        let norm = normalize("../../dir/file.json");
+        assert_eq!(norm, "../../dir/file.json");
+
+        let norm = normalize("../../dir/../file.json");
+        assert_eq!(norm, "../../file.json");
+    }
+}
