a240e226e8622ae09410122557d4cb8fecd0b385
Shandar Denys <msden.ua@gmail.com>
2024-03-23 19:46:16 +0200

    path concat and normalize

--- nanvm-lib/src/parser/path.rs
+++ nanvm-lib/src/parser/path.rs
@@ -3,6 +3,7 @@ pub fn normalize(path: &str) -> String {
     let mut result_split: Vec<&str> = Vec::new();
     for &dir in path_split.iter() {
         match dir {
+            "" => {}
             ".." => {
                 let last = result_split.last();
                 match last {
@@ -22,23 +23,39 @@ pub fn normalize(path: &str) -> String {
     result_split.join("/")
 }
 
-pub fn concat(_a: &str, _b: &str) -> String {
-    todo!()
+pub fn concat(a: &str, b: &str) -> String {
+    let mut concat = a.to_string();
+    concat.push('/');
+    concat.push_str(b);
+    normalize(&concat)
 }
 
 #[cfg(test)]
 mod test {
     use wasm_bindgen_test::wasm_bindgen_test;
 
-    use crate::parser::path::normalize;
+    use crate::parser::path::{concat, normalize};
 
     #[test]
     #[wasm_bindgen_test]
-    fn test() {
+    fn test_norm() {
         let norm = normalize("../../dir/file.json");
         assert_eq!(norm, "../../dir/file.json");
 
         let norm = normalize("../../dir/../file.json");
         assert_eq!(norm, "../../file.json");
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn test_concat() {
+        let result = concat("a", "b");
+        assert_eq!(result, "a/b");
+
+        let result = concat("a///b/", "c");
+        assert_eq!(result, "a/b/c");
+
+        let result = concat("a/../b/..", "c");
+        assert_eq!(result, "c");
     }
 }
