09c039eb17487544aece5b901e3a01259ccd529f
Shandar Denys <msden.ua@gmail.com>
2024-03-24 21:15:21 +0200

    run import example with real io

--- nanvm/src/main.rs
+++ nanvm/src/main.rs
@@ -3,11 +3,12 @@ use io_trait::Io;
 use nanvm_lib::parser::parse;
 use nanvm_lib::parser::path::concat;
 use nanvm_lib::{mem::local::Local, parser::Context};
+use nanvm_lib::serializer::WriteJson;
 
 fn main() {
     let local = Local::default();
     let io = RealIo();
-    let path = "../../nanvm-lib/test/test_import_main.d.cjs";
+    let path = "nanvm-lib/test/test_import_main.d.cjs";
     let context = Context::new(
         &local,
         &io,
@@ -15,11 +16,29 @@ fn main() {
     );
     let result = parse(&context);
     match result {
-        Ok(_) => println!("ok"),
-        Err(_) => print!("err"),
+        Ok(parse_result) => {
+            let mut s = String::new();
+            s.write_json(parse_result.any).unwrap();
+            println!("ok {}", s);
+        }
+        Err(err) => print!("err {:?}", err),
     }
 
-    //todo:
-    //1. read text file to string
-    //2. print json tokens from the string
+    let local = Local::default();
+    let io = RealIo();
+    let path = "nanvm-lib/test/test_import_main.d.mjs";
+    let context = Context::new(
+        &local,
+        &io,
+        concat(io.current_dir().unwrap().as_str(), path),
+    );
+    let result = parse(&context);
+    match result {
+        Ok(parse_result) => {
+            let mut s = String::new();
+            s.write_json(parse_result.any).unwrap();
+            println!("ok {}", s);
+        }
+        Err(err) => print!("err {:?}", err),
+    }
 }
