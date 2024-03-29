ce470fd2a84f81b33329dad15fc70cfd17bfafcc
Sasha Gil <alexgil@microsoft.com>
2024-03-29 08:08:49 -0700

    Split ConstBuilder off ConstTracker

--- nanvm-lib/src/serializer/to_djs.rs
+++ nanvm-lib/src/serializer/to_djs.rs
@@ -12,39 +12,52 @@ use super::to_json::WriteJson;
 
 use std::collections::{HashMap, HashSet};
 
-/// ConstTracker holds const-related data through following passes of djs serialization:
-/// 1. At the first pass we place a visited entity (an object or an array) first into
-///    `visited_once`, then (on the second visit) we move it to `to_do`. When the first pass
-///    is done, we drop `visited_once`, this set is needed only for populating `to_be_const`.
-/// 2. At the second pass we take entities from `to_do` and write them out as consts, also
-///    placing each written-out entity into done with its id. This is a depth-first pass, so
-///    at the moment when we write out an entity, all its const subordinates are already written out
-///    with their ids placed into `done_const`.
+/// ConstTracker holds references to js compounds (objects or arrays) in two sets:
+/// `visited_once` refers to compounds that we've seen just once so far;
+/// `visited_repeatedly` refers to compounds that we've seen more than once.
+/// When djs tracking pass is done, `visited_repeatedly` refers to compounds that will be written
+/// out via const definitions.
+/// Note that we use one ConstTracker for js objects and another for js arrays, keeping them
+/// separate - to reduce set sizes.
 struct ConstTracker<D: Dealloc> {
     visited_once: HashSet<Any<D>>,
-    to_do: HashSet<Any<D>>,
-    done: HashMap<Any<D>, usize>,
+    visited_repeatedly: HashSet<Any<D>>,
 }
 
 fn new_const_tracker<D: Dealloc>() -> ConstTracker<D> {
     ConstTracker {
         visited_once: HashSet::new(),
-        to_do: HashSet::new(),
+        visited_repeatedly: HashSet::new(),
+    }
+}
+
+/// ConstBuilder holds info on compounds that we write out as consts - initially having all
+/// referneces in `to_do`, moving them to `done` as we write them out one by one, with each
+/// compound written out after compounds it refers to (if any). Thus a const definition written
+/// out earlier can be used below by its name, and never above.
+struct ConstBuilder<D: Dealloc> {
+    to_do: HashSet<Any<D>>,
+    done: HashMap<Any<D>, usize>,
+}
+
+fn new_const_builder<D: Dealloc>(visited_repeatedly: HashSet<Any<D>>) -> ConstBuilder<D> {
+    ConstBuilder {
+        to_do: visited_repeatedly,
         done: HashMap::new(),
     }
 }
 
 /// Returns true if the `any` was visited before; updates the `const_tracker` set, tracking whether
 /// `any` was visited just once (it's in `const_tracker.visited_once`) or more than once (it's in
-/// `const_tracker.to_do_consts` in this case since we are up to writing it out as a const).
+/// `const_tracker.visited_repeatedly` in this case since we are up to writing it out as a const).
 fn is_visited<D: Dealloc>(any: Any<D>, const_tracker: &mut ConstTracker<D>) -> bool {
-    if const_tracker.to_do.contains(&any) {
+    if const_tracker.visited_repeatedly.contains(&any) {
         // We've visited `any` more than once before, no action is needed here.
         true
     } else if const_tracker.visited_once.contains(&any) {
         // It's the second time we visit `any`, move it from `visited_once` to `to_do`.
         const_tracker.visited_once.remove(&any);
-        const_tracker.to_do.insert(any);
+        const_tracker.visited_repeatedly.insert(any);
         true
     } else {
         // It's the first time we visit `any`, add it to `visited_once` (that is the only
@@ -124,13 +137,13 @@ fn collect_consts<D: Dealloc>(any: Any<D>) {
 /// written out in front of it.
 fn write_out_consts<D: Dealloc>(
     any: Any<D>,
-    object_const_tracker: &mut ConstTracker<D>,
+    _object_const_tracker: &mut ConstTracker<D>,
     _array_const_tracker: &mut ConstTracker<D>,
 ) {
     match any.get_type() {
         Type::Object => {
-            let opt_k_v = object_const_tracker.done.get_key_value(&any);
-            if opt_k_v.is_some() {}
+            //let opt_k_v = object_const_tracker.done.get_key_value(&any);
+            //if opt_k_v.is_some() {}
         }
         _ => {}
     }
